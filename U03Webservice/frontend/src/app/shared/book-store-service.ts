7c62209586fb7f376de075c5fd06056bcde3fc74
Benji <50681275+Benji377@users.noreply.github.com>
2023-02-01 17:51:49 +0100

    New project

---
+++ U03Webservice/frontend/src/app/shared/book-store-service.ts
@@ -0,0 +1,13 @@
+import { Book } from "./book";
+
+export class BookStoreService {
+  resetStore() { }
+  getAllBooks() { }
+  getAllBooksSearchTerm(searchTerm) { }
+  createBook(book) { }
+  deleteBook(isbn) { }
+  getBook(isbn) { }
+  updateBook(book) { }
+  checkBook(isbn) { }
+  rateBook(isbn, rating) { }
+}
